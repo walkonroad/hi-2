4017265959f8180cc3c030411dd2af5fe6e5f300
free <shuaipengmail@163.com>
2018-10-11 11:01:10 +0800

    add

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class App {
+
+	@RequestMapping("/")
+	public String sayHi(String name) {
+		return "版本2";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+}
